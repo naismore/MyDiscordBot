fb0dbef1c1cc0b009f89b1351c095f7a0b01583b
naismore <anxismore@gmail.com>
2024-05-06 21:07:45 +0300

    Основа для бота Discord + Embed

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using MyDiscordBot.commands;
+using MyDiscordBot.config;
+
+namespace AnxisBot
+{
+    class Bot
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+            Client.GuildMemberAdded += MemberAddedHandler;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<TestCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task MemberAddedHandler(DiscordClient sender, DSharpPlus.EventArgs.GuildMemberAddEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ commands/TestCommands.cs
@@ -0,0 +1,43 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace MyDiscordBot.commands
+{
+    public class TestCommands : BaseCommandModule
+    {
+        [Command("test")]
+        public async Task MyFirstCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"Hello {ctx.User.Mention}");
+        }
+
+        [Command("add")]
+        public async Task Add(CommandContext ctx, int number1, int number2)
+        {
+            await ctx.Channel.SendMessageAsync($"{number1} + {number2} = {number1 + number2}");
+        }
+        [Command("embed")]
+        public async Task EmbedMessage(CommandContext ctx)
+        {
+            var message = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+                .WithTitle("This My first Discord Embed")
+                .WithDescription($"This command was executed by {ctx.User.Mention}")
+                .WithColor(DiscordColor.Red));
+            await ctx.Channel.SendMessageAsync(message);
+        }
+
+        [Command("embed1")]
+        public async Task Embed1Message(CommandContext ctx)
+        {
+            var message = new DiscordEmbedBuilder
+            {
+                Title = "This My first Discord Embed",
+                Description = $"This command was executed by {ctx.User.Mention}",
+                Color = DiscordColor.Red,
+            };
+            await ctx.Channel.SendMessageAsync(message);
+        }
+    }
+}
